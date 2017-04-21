cfd31744d57678b9cdc5b8a2117c1cbe2d80e667
yangfu <1204914685@qq.com>
2017-04-21 23:29:15 +0800

    4
    4

--- demo/js/chart.js
+++ demo/js/chart.js
@@ -2,27 +2,27 @@ var myChart = echarts.init(document.getElementById('main'));
 var  colors=['#d53a35','#2f4455','#639fa9'];
 var i = 0;
 option = {
-    tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b}: {c} ({d}%)"
-    },
-    title :
-    {
+    baseOption: {
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+    title: {
         text: '本月受理:100次'
     },
     legend: {
         orient: 'horizontal',
         x: 'left',
-        y:'bottom',
-        data:['已回复','正在处理','未回复'],
+        y: 'bottom',
+        data: ['已回复', '正在处理', '未回复'],
 
     },
     series: [
         {
-            name:'访问来源',
-            type:'pie',
+            name: '访问来源',
+            type: 'pie',
             radius: ['50%', '70%'],
-            avoidLabelOverlap:false,
+            avoidLabelOverlap: false,
             label: {
                 normal: {
                     show: false,
@@ -36,16 +36,16 @@ option = {
                     }
                 }
             },
-            itemStyle : {
-                normal : {
-                    label : {
-                        show : false
+            itemStyle: {
+                normal: {
+                    label: {
+                        show: false
                     },
-                    labelLine : {
-                        show : false
+                    labelLine: {
+                        show: false
                     },
                     //自定义颜色数组
-                    color:function(){
+                    color: function () {
                         return colors[i++];
                     }
                 }
@@ -56,13 +56,52 @@ option = {
                     show: true
                 }
             },
-            data:[
-                {value:160, name:'已回复'},
-                {value:60, name:'正在处理'},
-                {value:30, name:'未回复'}
+            data: [
+                {value: 160, name: '已回复'},
+                {value: 60, name: '正在处理'},
+                {value: 30, name: '未回复'}
             ]
         }
     ]
+  },
+    media:[
+        {
+            query: {
+                minWidth: 200
+            },
+            option: {
+                legend: {
+                    orient: 'horizontal' ,
+                    left:'10',
+                    bottom:'0'
+                }
+            },
+            series: [
+            {
+                radius: ['40%', '40%']
+            }
+        ]
+        },
+        {
+            query: {
+                minWidth: 300
+            },
+            option: {
+                legend: {
+                    orient: 'horizontal' ,
+                    left:'40',
+                    bottom:'0'
+                }
+            },
+            series: [
+                {
+                    radius: ['50%', '50%']
+                }
+            ]
+        }
+
+    ]
+
 };
 myChart.setOption(option);
 
