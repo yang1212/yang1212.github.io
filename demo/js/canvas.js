ef4031132b12128f58296592f12050029962670a
yangfu <1204914685@qq.com>
2017-04-24 20:59:39 +0800

    1
    1

--- demo/js/canvas.js
+++ demo/js/canvas.js
@@ -1,172 +1,84 @@
-var canvas,
-    ctx,
-    width,
-    height,
-    size,
-    lines,
-    tick;
-
-function line() {
-    this.path = [];
-    this.speed = rand(10, 20);
-    this.count = randInt(10, 30);
-    this.x = width / 2, +1;
-    this.y = height / 2 + 1;
-    this.target = {
-        x: width / 2,
-        y: height / 2
-    };
-    this.dist = 0;
-    this.angle = 0;
-    this.hue = tick / 5;
-    this.life = 1;
-    this.updateAngle();
-    this.updateDist();
-}
-
-line.prototype.step = function(i) {
-    this.x += Math.cos(this.angle) * this.speed;
-    this.y += Math.sin(this.angle) * this.speed;
-
-    this.updateDist();
-
-    if (this.dist < this.speed) {
-        this.x = this.target.x;
-        this.y = this.target.y;
-        this.changeTarget();
-    }
-
-    this.path.push({
-        x: this.x,
-        y: this.y
-    });
-    if (this.path.length > this.count) {
-        this.path.shift();
-    }
-
-    this.life -= 0.001;
-
-    if (this.life <= 0) {
-        this.path = null;
-        lines.splice(i, 1);
-    }
-};
-
-line.prototype.updateDist = function() {
-    var dx = this.target.x - this.x,
-        dy = this.target.y - this.y;
-    this.dist = Math.sqrt(dx * dx + dy * dy);
-}
-
-line.prototype.updateAngle = function() {
-    var dx = this.target.x - this.x,
-        dy = this.target.y - this.y;
-    this.angle = Math.atan2(dy, dx);
-}
-
-line.prototype.changeTarget = function() {
-    var randStart = randInt(0, 3);
-    switch (randStart) {
-        case 0: // up
-            this.target.y = this.y - size;
-            break;
-        case 1: // right
-            this.target.x = this.x + size;
-            break;
-        case 2: // down
-            this.target.y = this.y + size;
-            break;
-        case 3: // left
-            this.target.x = this.x - size;
-    }
-    this.updateAngle();
-};
-
-line.prototype.draw = function(i) {
-    ctx.beginPath();
-    var rando = rand(0, 10);
-    for (var j = 0, length = this.path.length; j < length; j++) {
-        ctx[(j === 0) ? 'moveTo' : 'lineTo'](this.path[j].x + rand(-rando, rando), this.path[j].y + rand(-rando, rando));
-    }
-    ctx.strokeStyle = 'hsla(' + rand(this.hue, this.hue + 30) + ', 80%, 55%, ' + (this.life / 3) + ')';
-    ctx.lineWidth = rand(0.1, 2);
-    ctx.stroke();
-};
-
-function rand(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function randInt(min, max) {
-    return Math.floor(min + Math.random() * (max - min + 1));
-};
-
-function init() {
-    canvas = document.getElementById('cas');
-    ctx = canvas.getContext('2d');
-    size = 30;
-    lines = [];
-    reset();
-    loop();
-}
-
-function reset() {
-    width = Math.ceil(window.innerWidth / 2) * 2;
-    height = Math.ceil(window.innerHeight / 2) * .49;
-    tick = 0;
-
-    lines.length = 0;
-    canvas.width = width;
-    canvas.height = height;
-}
-
-function create() {
-    if (tick % 10 === 0) {
-        lines.push(new line());
-    }
-}
-
-function step() {
-    var i = lines.length;
-    while (i--) {
-        lines[i].step(i);
-    }
-}
-
-function clear() {
-    ctx.globalCompositeOperation = 'destination-out';
-    ctx.fillStyle = 'hsla(0, 0%, 0%, 0.1';
-    ctx.fillRect(0, 0, width, height);
-    ctx.globalCompositeOperation = 'lighter';
-}
-
-function draw() {
-    ctx.save();
-    ctx.translate(width / 2, height / 2);
-    ctx.rotate(tick * 0.001);
-    var scale = 0.8 + Math.cos(tick * 0.02) * 0.2;
-    ctx.scale(scale, scale);
-    ctx.translate(-width / 2, -height / 2);
-    var i = lines.length;
-    while (i--) {
-        lines[i].draw(i);
-    }
-    ctx.restore();
-}
-
-function loop() {
-    requestAnimationFrame(loop);
-    create();
-    step();
-    clear();
-    draw();
-    tick++;
-}
-
-function onresize() {
-    reset();
-}
-
-window.addEventListener('resize', onresize);
-
+var SEPARATION = 180, AMOUNTX = 30, AMOUNTY = 30;
+var camera, scene, renderer;
+var particles, particle, count = 0;
+var mouseX = 0, mouseY = 0;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = 280 / 2;
 init();
+animate();
+function init() {
+    var container = document.getElementById( 'cas' );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / 280, 1, 10000 );
+    camera.position.z = 1000;
+    scene = new THREE.Scene();
+    particles = new Array();
+    var PI2 = Math.PI * 2;
+    var material = new THREE.ParticleCanvasMaterial( {
+        color: 0xff0000,
+        program: function ( context ) {
+            context.beginPath();
+            context.arc( 0, 0, 1.1, 0, PI2, true );
+            context.fill();
+        }
+    } );
+    var i = 0;
+    for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
+        for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
+            particle = particles[ i ++ ] = new THREE.Particle( material );
+            particle.position.x = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 );
+            particle.position.z = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 );
+            scene.add( particle );
+        }
+    }
+    renderer = new THREE.CanvasRenderer();
+    renderer.setSize( window.innerWidth, 290 );
+    container.appendChild( renderer.domElement );
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+    window.addEventListener( 'resize', onWindowResize, false );
+}
+function onWindowResize() {
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = 280 / 2;
+    camera.aspect = window.innerWidth / 280;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, 280  );
+}
+function onDocumentMouseMove( event ) {
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+}
+function onDocumentTouchStart( event ) {
+    if ( event.touches.length === 1 ) {
+        event.preventDefault();
+        mouseX = event.touches[ 0 ].pageX - windowHalfX;
+        mouseY = event.touches[ 0 ].pageY - windowHalfY;
+    }
+}
+function onDocumentTouchMove( event ) {
+    if ( event.touches.length === 1 ) {
+        event.preventDefault();
+        mouseX = event.touches[ 0 ].pageX - windowHalfX;
+        mouseY = event.touches[ 0 ].pageY - windowHalfY;
+    }
+}
+function animate() {
+    requestAnimationFrame( animate );
+    render();
+}
+function render() {
+    camera.position.x += ( mouseX - camera.position.x ) * .05;
+    camera.position.y += ( - mouseY - camera.position.y ) * .05;
+    camera.lookAt( scene.position );
+    var i = 0;
+    for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
+        for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
+            particle = particles[ i++ ];
+            particle.position.y = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) + ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
+            particle.scale.x = particle.scale.y = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 2 + ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 2;
+        }
+    }
+    renderer.render( scene, camera );
+    count += 0.1;
+}
