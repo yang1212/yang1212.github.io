cfb7965fe30554436d6839837b54db56a4a5ed97
杨伏 <1204914685@qq.com>
2025-05-22 10:13:34 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   base: '/yang1212.github.io',
   publicPath: './',
-  outputDir: 'docs',
   devServer: {
     host: '0.0.0.0', // 或者设置为你的服务器的实际 IP 地址
     port: 8080,
